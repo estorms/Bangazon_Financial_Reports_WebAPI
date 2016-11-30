f5a1e1eadc36c69eb2aa4147a1eba5fc7e602f0e
Elizabeth Sanger <libsang@gmail.com>
2016-11-29 18:43:55 -0600

    refactored to make gets async

--- WEB-API/Controllers/RevenueController.cs
+++ WEB-API/Controllers/RevenueController.cs
@@ -15,6 +15,13 @@ namespace BangazonFinancialReportsAPI
 
         private BangazonContext context;
 
+        Revenue testRevenue = new Revenue
+        {
+            ProductName = "Tough Luck",
+            ProductCost = 5,
+        };
+
+
 
         public RevenueController(BangazonContext ctx)
         {
@@ -24,98 +31,130 @@ namespace BangazonFinancialReportsAPI
 
         [HttpGet]
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            IQueryable<Revenue> AllRevenue = from revenue in context.Revenue select revenue;
+            var BangazonRevenue = context.Revenue;
+            IQueryable<Revenue> AllRevenue = from revenue in BangazonRevenue select revenue;
 
-            if (AllRevenue == null)
-            {
-                return NotFound(); //produces a 404 response
-            }
-// 
+            // if (AllRevenue == null)
+            // {
+            //     return NotFound(); //produces a 404 response
+            // }
+            // // 
             return Ok(AllRevenue); //http response of status 200 and return to client
 
         }
 
+        //       public IActionResult ProdRevenue()
+        //         {
+        //             IQueryable<object> ProdsRevenue = from revenue in context.Revenue 
+        //             group revenue by revenue.ProductName;
+        //             // select new {revenue.ProductName, revenue.ProductCost};
+
+        //             if (ProdsRevenue == null)
+        //             {
+        //                 return NotFound(); //produces a 404 response
+        //             }
+        // // 
+        //             return Ok(ProdsRevenue); //http response of status 200 and return to client
+
+        //         }
 
         [HttpGet]
         public IActionResult ProductRevenue()
         {
 
-            Revenue testRevenue = new Revenue {
-                ProductName = "Tough Luck",
-                ProductCost = 5,
-            };
 
             var BangazonRevenue = context.Revenue;
 
-            try
-            {
-                var ProdsRevenue =
-                from revenue in BangazonRevenue
-                
-                select new {revenue.ProductCost, revenue.ProductName};
 
-            
-        
-                // orderby t.ProductCost
-                // orderby ProductNames
-                
+            //group revenue items by ProductName to avoid duplicates
+            var ProductGrouper = from revenue in BangazonRevenue
+                                 group revenue by revenue.ProductName into groupedRevenueProductNames
+                                 where groupedRevenueProductNames.Count() >= 1
+                                 select new { groupedRevenueProductNames.Key };
 
-          
-// from Revenue
-    
-//             group by Revenue.ProductName
-//             order by sum(Revenue.ProductRevenue) desc",  
-    
-                // IQueryable<string> ProductNames = from revenue in context.Revenue select revenue.ProductName;
-                // IQueryable<int>ProductCosts = from revenue in context.Revenue select revenue.ProductCost;
-            // .Include(x => x.ProductRevenue)
-            //     .OrderBy(x => x.ProductRevenue)
-                
-                // Dictionary <string, int> ProdsRevenue = new Dictionary<string, int>();
-                // ProdsRevenue.Add(ProductNames.ToString(), Convert.ToInt32(ProductCosts));
 
-                if (ProdsRevenue == null)
-                {
-                    return NotFound();
-                }
 
-                return Ok(ProdsRevenue);
-            }
+            var RevenueGrouper = from revenue in BangazonRevenue
+                                 group revenue by revenue.ProductCost into groupedRevenueProductCosts
+                                 where groupedRevenueProductCosts.Count() >= 1
+                                 orderby Convert.ToInt32(groupedRevenueProductCosts)
+                                 select new { groupedRevenueProductCosts.Key };
 
-            catch (System.InvalidOperationException ex)
-            {
-                return NotFound();
+            // foreach(var p in ProductGrouper) {
+            //     Console.WriteLine(p);
+            // }
+            // Get line items grouped by product id, including count
 
-            }
+            // Build list of Product instances for display in view
+            // model.ProductTypes = await (from type in context.ProductType
+            //         join a in counter on type.ProductTypeId equals a.Key 
+            //         select new ProductType {
+            //             ProductTypeId = type.ProductTypeId,
+            //             Label = type.Label, 
+            //             Quantity = a.myCount 
+            //         }).ToListAsync();
+
+
+
+            return Ok(RevenueGrouper);
         }
-          
-      [HttpGet("{id}", Name = "GetIndividualRevenue")]
-        public IActionResult Index([FromRoute] int id)
+
+
+
+
+
+
+
+
+
+
+        [HttpGet("{id}", Name = "GetIndividualRevenue")]
+            public async Task<IActionResult> Index([FromRoute]int? id)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            try
-            {
-                Revenue revenue = context.Revenue.Single(m => m.Id == id);
-
-                if (revenue == null)
-                {
-                    return NotFound();
-                }
-                
-                return Ok(revenue);
-            }
-            catch (System.InvalidOperationException ex)
+            // If no id was in the route, return 404
+            if (id == null)
             {
                 return NotFound();
             }
 
+          
+            // Set the `Product` property of the view model
+            Revenue AllRevenue = await context.Revenue
+                
+                    .SingleOrDefaultAsync(prod => prod.Id == id);
 
+            // If product not found, return 404
+            if (AllRevenue == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(AllRevenue); 
         }
     }
 }
+
+
+
+
+// // from Revenue
+
+//                 // IQueryable<string> ProductNames = from revenue in context.Revenue select revenue.ProductName;
+//                 // IQueryable<int>ProductCosts = from revenue in context.Revenue select revenue.ProductCost;
+//             // .Include(x => x.ProductRevenue)
+//             //     .OrderBy(x => x.ProductRevenue)
+
+//                 // Dictionary <string, int> ProdsRevenue = new Dictionary<string, int>();
+//                 // ProdsRevenue.Add(ProductNames.ToString(), Convert.ToInt32(ProductCosts));
+// foreach (var entry in ProdsRevenue) {
+//     ProductsDictionary.Add(entry.ProductName, entry.ProductCost);
+// }
+// @"Select Revenue.ProductName,
+//             sum(Revenue.ProductRevenue) 
+//             from Revenue
+//             group by Revenue.ProductName
+//             order by sum(Revenue.ProductRevenue) desc", 
+// List<KeyValuePair<string, int>>ProductsKVPairList = new List<KeyValuePair<string, int>>();
+// Dictionary<string, int>ProductsDictionary = new Dictionary<string, int>();
